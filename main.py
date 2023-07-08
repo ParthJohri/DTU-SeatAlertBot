269b78066344e900843197415028348ef650ab5a
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-07-08 22:30:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 import pymongo
+from telegram.error import TelegramError
 
 load_dotenv()
 
@@ -31,19 +32,62 @@ data=""
 client = pymongo.MongoClient(f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@cluster0.1e3sslc.mongodb.net/?retryWrites=true&w=majority")
 db = client.subjectRegistration
 give_take_collection=db.giveTake
+whatsapp_collection=db.whatsappLinks
 users_collection = db.users
 subject_seats_collection = db.subjectSeats
 
-most_recent_document = subject_seats_collection.find_one({}, sort=[('$natural', -1)])
+most_recent_document = subject_seats_collection.find_one(filter={"$and": [{"$nor": [{"_id": None}]}, {"_id": {"$ne": None}}]}, sort=[("_id", -1)])
 course_map = dict(most_recent_document) if most_recent_document else {}
 
-second_last_document = subject_seats_collection.find_one({}, sort=[('$natural', -1)], skip=1)
+second_last_document = subject_seats_collection.find_one(filter={"$and": [{"$nor": [{"_id": None}]}, {"_id": {"$ne": None}}]}, sort=[("_id", -1)], skip=1)
 previous_map = dict(second_last_document) if second_last_document else {}
 
 
 user_ids_dict = list(users_collection.find({}, {"_id": 0, "user_id": 1}))
 user_ids = list(map(lambda item: item["user_id"], user_ids_dict))
 
+
+@bot.message_handler(commands=['addwa'])
+def addwa(message):
+    user_id = message.chat.id
+    bot.reply_to(message, '<b>Please provide your subject code.</b>', parse_mode="HTML")
+    bot.register_next_step_handler(message, lambda msg: addlink(msg, user_id))
+
+def addlink(message, user_id):
+    give_value = message.text.upper()
+    bot.reply_to(message, '<b>Please provide the corresponding Whatsapp Group Link.</b>', parse_mode="HTML")
+    bot.register_next_step_handler(message, lambda msg: processlink(msg, user_id, give_value))
+
+def processlink(message, user_id, give_value):
+    link_info = message.text
+    user = {
+        "give_value": give_value,
+        "whatsapplink": link_info
+    }
+    whatsapp_collection.insert_one(user)
+    bot.send_message(user_id, "<b>Whatsapp Group Added!</b>",parse_mode="HTML")
+
+@bot.message_handler(commands=['getwa'])
+def getwa(message):
+    user_id = message.chat.id
+    bot.reply_to(message, '<b>Please provide the subject code.</b>', parse_mode="HTML")
+    bot.register_next_step_handler(message, lambda msg: process_getwa(msg, user_id))
+
+def process_getwa(message, user_id):
+    subject_code = message.text.upper()
+    links = []
+    cursor = whatsapp_collection.find({"give_value": subject_code})
+    for document in cursor:
+        link_info = document.get("whatsapplink")
+        links.append(link_info)
+    
+    if links:
+        response = f"<b>WhatsApp links for subject {subject_code}:</b>\n"
+        for link in links:
+            response += f"- {link}\n"
+        bot.send_message(user_id, response, parse_mode="HTML")
+    else:
+        bot.send_message(user_id, f"No WhatsApp links found for subject {subject_code}.", parse_mode="HTML")
 
 @bot.message_handler(commands=['register'])
 def register(message):
@@ -58,7 +102,7 @@ def process_give(message, user_id):
 
 def process_take(message, user_id, give_value):
     take_value = message.text.upper()
-    bot.reply_to(message, '<b>Please provide your contact info.</b>', parse_mode="HTML")
+    bot.reply_to(message, '<b>Please provide your contact info either your mail or telegram username or number</b>', parse_mode="HTML")
     bot.register_next_step_handler(message, lambda msg: process_contact_info(msg, user_id, give_value, take_value))
 
 def process_contact_info(message, user_id, give_value, take_value):
@@ -163,8 +207,11 @@ def notify(user_id, data):
   user_ids = list(map(lambda item: item["user_id"], user_ids_dict))
   print(user_ids)
   for user_id in user_ids:
+    try:
         bot.send_message(user_id, data, parse_mode="HTML")
-
+    except TelegramError as e:
+        users_collection.delete_many({"user_id":user_id})
+            
 @bot.message_handler(commands=['revoke'])
 def revoke_permission(message):
   global user_ids
@@ -190,8 +237,11 @@ def handle_start(message):
         "🔎 <b>/search -</b> Get seat count for a subject.",
         "✏️ <b>/register -</b> Register for subject exchange.",
         "🚫 <b>/unregister -</b> Unregister from subject exchange.",
-        "💡 <b>/exchange -</b> Check subject exchange matches."
-    ]
+        "💡 <b>/exchange -</b> Check subject exchange matches.",
+        "🔗 <b>/addwa -</b> Add WhatsApp link to a subject.",
+        "🔗 <b>/getwa -</b> Get WhatsApp links of a subject.",
+        "<b>PS: Write Subject Code like this CO203</b>"
+    ]   
 
     commands_text = '\n'.join(commands)
 
@@ -211,7 +261,7 @@ def scrape_function():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    service = Service(executable_path=r'/usr/local/bin/chromedriver')
+    service = Service(executable_path=r'/usr/bin/chromedriver')
     driver = webdriver.Chrome(service=service, options=chrome_options)
     loginurl = ('https://cumsdtu.in/registration_student/login/login.jsp?courseRegistration')
 
@@ -221,9 +271,25 @@ def scrape_function():
     usernameId = str(os.environ['usernameId'])
     passwordId = str(os.environ['passwordId'])
     
-    driver.get(loginurl)
-   
-    print(loginurl);
+    max_retries = 3
+
+    for retry in range(max_retries):
+        try:
+            # Navigate to the URL
+            driver.get(loginurl)
+
+            # Check for error message
+            error_message = "Invalid User name or password. Please check."
+            if error_message in driver.page_source:
+                raise WebDriverException(error_message)
+
+            # If no error, break out of the loop
+            break
+        except WebDriverException as e:
+            # Handle the error
+            print(f"Error: {e}")
+            print("Retrying...")
+
     RN = driver.find_element(
       By.ID, 'usernameId')
     RN.send_keys(usernameId)
@@ -236,7 +302,10 @@ def scrape_function():
       By.ID, "submitButton"
     )  
     submit_button.send_keys(Keys.ENTER)
-    time.sleep(5)
+    
+    wait = WebDriverWait(driver, 120)  # Maximum wait time in seconds
+    element_locator = (By.CSS_SELECTOR, "div.course.isDisabled")
+    element = wait.until(EC.visibility_of_element_located(element_locator))
 
     page_source = driver.page_source
 
@@ -258,44 +327,42 @@ def scrape_function():
 
         subject_seats_dict[subject_code] = seats
 
-    subject_seats_dict_size = len(subject_seats_dict)
-
-    if subject_seats_dict_size == 1:
-        scrape_function()
+    flag =True;
+    if not subject_seats_dict:  
+        print("Empty document")
+        flag=False;
+    else:
+        subject_seats_collection.insert_one(subject_seats_dict)
 
     document =  subject_seats_dict
     
-    subject_seats_collection.insert_one(document)
-    
-    course_map = {subject_code: seats for subject_code, seats in document.items()}
-    
-    most_recent_document = None if subject_seats_collection.count_documents({}) < 1 else subject_seats_collection.find_one({}, sort=[('$natural', -1)])
+    if flag == True:
+        course_map = {subject_code: seats for subject_code, seats in document.items()}
 
-    course_map = {} if most_recent_document == None else dict(most_recent_document)
-    
-    notification_message = "<b>✨ Seats Availability Update</b>\n --------------------------------------------- \n"
-    
-    count = 0
+        notification_message = "<b>✨ Seats Availability Update</b>\n --------------------------------------------- \n"
+        
+        count = 0
 
-    for subject_code, current_seats in course_map.items():
-        if subject_code == "_id" or subject_code == "Set" or subject_code == "Group":
-            continue
-        previous_seats = previous_map.get(subject_code)
-        if previous_seats is None or (previous_seats is not None and current_seats != previous_seats):
-                count += 1
-                x = f"<b>{subject_code}</b> -> <b>{current_seats}</b>."
-                notification_message += x + "\n"
+        for subject_code, current_seats in course_map.items():
+            if subject_code == "_id" or subject_code == "Set" or subject_code == "Group":
+                continue
+            previous_seats = previous_map.get(subject_code)
+            if previous_seats is None or (previous_seats is not None and current_seats != previous_seats):
+                    count += 1
+                    x = f"<b>{subject_code}</b> -> <b>{current_seats}</b>."
+                    notification_message += x + "\n"
 
-    if count != 0:
-        notify(user_ids, notification_message)
+        if count != 0:
+            notify(user_ids, notification_message)
+                    
+        previous_map = course_map
+        
+        course_map_string = "<b>Subject Codes: Seats</b>\n"
+        for subject_code, seats in course_map.items():
+            if seats != "0" and seats!="Not available" and subject_code != "_id" and subject_code != "Group" and subject_code != "Set":
+                course_map_string += f"<b>{subject_code}</b>: {seats}\n"
                 
-    previous_map = course_map
-    
-    course_map_string = "<b>Subject Codes: Seats</b>\n"
-    for subject_code, seats in course_map.items():
-        if seats != "0" and seats!="Not available" and subject_code != "_id" and subject_code != "Group" and subject_code != "Set":
-            course_map_string += f"<b>{subject_code}</b>: {seats}\n"
-    data = course_map_string;
+        data = course_map_string;
     
     driver.quit()
 
@@ -305,7 +372,7 @@ def task_function():
   while True:
       try:
         scrape_function()
-        time.sleep(20)
+        time.sleep(5)
       except Exception as e:
         print(e)
         time.sleep(5)
